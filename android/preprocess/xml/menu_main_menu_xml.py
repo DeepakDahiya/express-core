374d280df9a0aab6a7089be380135a0ed0476a8e
deepakdahiya <dahiyadeepak123@gmail.com>
2023-11-22 15:56:21 +0530

    cleaning up menu bar

--- android/preprocess/xml/menu_main_menu_xml.py
+++ android/preprocess/xml/menu_main_menu_xml.py
@@ -14,44 +14,46 @@ def _ProcessXML(root):
     for prefix, uri in ns.items():
         ET.register_namespace(prefix, uri)
 
-    node_str = '<item xmlns:android='\
-    '"http://schemas.android.com/apk/res/android" '\
-      'android:id="@+id/brave_wallet_id" ' \
-      'android:title="@string/menu_brave_wallet" ' \
-      'android:visibility="gone" />'
-    node = ET.fromstring(node_str, parser=ET.XMLParser(encoding="utf-8"))
+    ## Removing Leo From Menu 
+    # node_str = '<item xmlns:android='\
+    # '"http://schemas.android.com/apk/res/android" '\
+    #   'android:id="@+id/brave_wallet_id" ' \
+    #   'android:title="@string/menu_brave_wallet" ' \
+    #   'android:visibility="gone" />'
+    # node = ET.fromstring(node_str, parser=ET.XMLParser(encoding="utf-8"))
 
-    parent = root.find('group/[@android:id="@+id/PAGE_MENU"]', namespaces=ns)
-    child = parent.find('item/[@android:id="@+id/all_bookmarks_menu_id"]',
-                        namespaces=ns)
-    idx = list(parent).index(child)
-    parent.insert(idx + 1, node)
+    # parent = root.find('group/[@android:id="@+id/PAGE_MENU"]', namespaces=ns)
+    # child = parent.find('item/[@android:id="@+id/all_bookmarks_menu_id"]',
+    #                     namespaces=ns)
+    # idx = list(parent).index(child)
+    # parent.insert(idx + 1, node)
 
-    playlist_node_str = '<item xmlns:android='\
-    '"http://schemas.android.com/apk/res/android" '\
-      'android:id="@+id/brave_playlist_id" ' \
-      'android:title="@string/brave_playlist" />'
-    playlist_node = ET.fromstring(playlist_node_str,
-                                  parser=ET.XMLParser(encoding="utf-8"))
+    # playlist_node_str = '<item xmlns:android='\
+    # '"http://schemas.android.com/apk/res/android" '\
+    #   'android:id="@+id/brave_playlist_id" ' \
+    #   'android:title="@string/brave_playlist" />'
+    # playlist_node = ET.fromstring(playlist_node_str,
+    #                               parser=ET.XMLParser(encoding="utf-8"))
 
-    parent = root.find('group/[@android:id="@+id/PAGE_MENU"]', namespaces=ns)
-    child = parent.find('item/[@android:id="@+id/brave_wallet_id"]',
-                        namespaces=ns)
-    idx = list(parent).index(child)
-    parent.insert(idx + 1, playlist_node)
+    # parent = root.find('group/[@android:id="@+id/PAGE_MENU"]', namespaces=ns)
+    # child = parent.find('item/[@android:id="@+id/brave_wallet_id"]',
+    #                     namespaces=ns)
+    # idx = list(parent).index(child)
+    # parent.insert(idx + 1, playlist_node)
 
-    brave_leo_node_str = '<item xmlns:android='\
-        '"http://schemas.android.com/apk/res/android" '\
-          'android:id="@+id/brave_leo_id" ' \
-          'android:title="@string/menu_brave_leo" />'
-    brave_leo_node = ET.fromstring(brave_leo_node_str,
-                                   parser=ET.XMLParser(encoding="utf-8"))
+    ## Removing Leo From Menu 
+    # brave_leo_node_str = '<item xmlns:android='\
+    #     '"http://schemas.android.com/apk/res/android" '\
+    #       'android:id="@+id/brave_leo_id" ' \
+    #       'android:title="@string/menu_brave_leo" />'
+    # brave_leo_node = ET.fromstring(brave_leo_node_str,
+    #                                parser=ET.XMLParser(encoding="utf-8"))
 
-    parent = root.find('group/[@android:id="@+id/PAGE_MENU"]', namespaces=ns)
-    child = parent.find('item/[@android:id="@+id/brave_playlist_id"]',
-                        namespaces=ns)
-    idx = list(parent).index(child)
-    parent.insert(idx + 1, brave_leo_node)
+    # parent = root.find('group/[@android:id="@+id/PAGE_MENU"]', namespaces=ns)
+    # child = parent.find('item/[@android:id="@+id/brave_playlist_id"]',
+    #                     namespaces=ns)
+    # idx = list(parent).index(child)
+    # parent.insert(idx + 1, brave_leo_node)
 
     set_as_default_node_str = '<item xmlns:android='\
     '"http://schemas.android.com/apk/res/android" '\
@@ -64,36 +66,36 @@ def _ProcessXML(root):
     set_as_default_idx = list(parent).index(set_as_default_child)
     parent.insert(set_as_default_idx + 1, set_as_default_node)
 
-    vpn_node_str = '<item xmlns:android='\
-    '"http://schemas.android.com/apk/res/android" '\
-            'android:id="@+id/request_brave_vpn_row_menu_id" '\
-            'android:title="@null"> '\
-            '<menu> '\
-                '<item android:id="@+id/request_brave_vpn_id" '\
-                  'android:title="@string/brave_vpn" '\
-                  'android:icon="@drawable/ic_vpn" /> '\
-                '<item android:id="@+id/request_brave_vpn_check_id" '\
-                  'android:title="@null" '\
-                  'android:checkable="true" /> '\
-            '</menu> '\
-            '</item>'
-    vpn_node = ET.fromstring(vpn_node_str,
-                             parser=ET.XMLParser(encoding="utf-8"))
-    vpn_child = parent.find('item/[@android:id="@+id/set_default_browser"]',
-                            namespaces=ns)
-    vpn_idx = list(parent).index(vpn_child)
-    parent.insert(vpn_idx + 1, vpn_node)
+    # vpn_node_str = '<item xmlns:android='\
+    # '"http://schemas.android.com/apk/res/android" '\
+    #         'android:id="@+id/request_brave_vpn_row_menu_id" '\
+    #         'android:title="@null"> '\
+    #         '<menu> '\
+    #             '<item android:id="@+id/request_brave_vpn_id" '\
+    #               'android:title="@string/brave_vpn" '\
+    #               'android:icon="@drawable/ic_vpn" /> '\
+    #             '<item android:id="@+id/request_brave_vpn_check_id" '\
+    #               'android:title="@null" '\
+    #               'android:checkable="true" /> '\
+    #         '</menu> '\
+    #         '</item>'
+    # vpn_node = ET.fromstring(vpn_node_str,
+    #                          parser=ET.XMLParser(encoding="utf-8"))
+    # vpn_child = parent.find('item/[@android:id="@+id/set_default_browser"]',
+    #                         namespaces=ns)
+    # vpn_idx = list(parent).index(vpn_child)
+    # parent.insert(vpn_idx + 1, vpn_node)
 
-    speedreader_node_str = '<item xmlns:android='\
-            '"http://schemas.android.com/apk/res/android" '\
-            'android:id="@+id/brave_speedreader_id" '\
-            'android:title="@string/brave_speedreader_title" '\
-            'android:icon="@drawable/ic_readermode"/>'
-    speedreader_node = ET.fromstring(speedreader_node_str,
-                                     parser=ET.XMLParser(encoding="utf-8"))
-    speedreader_child = parent.find('item/[@android:id="@+id/page_zoom_id"]',
-                                    namespaces=ns)
-    speedreader_idx = list(parent).index(speedreader_child)
-    parent.insert(speedreader_idx, speedreader_node)
+    # speedreader_node_str = '<item xmlns:android='\
+    #         '"http://schemas.android.com/apk/res/android" '\
+    #         'android:id="@+id/brave_speedreader_id" '\
+    #         'android:title="@string/brave_speedreader_title" '\
+    #         'android:icon="@drawable/ic_readermode"/>'
+    # speedreader_node = ET.fromstring(speedreader_node_str,
+    #                                  parser=ET.XMLParser(encoding="utf-8"))
+    # speedreader_child = parent.find('item/[@android:id="@+id/page_zoom_id"]',
+    #                                 namespaces=ns)
+    # speedreader_idx = list(parent).index(speedreader_child)
+    # parent.insert(speedreader_idx, speedreader_node)
 
     return root
