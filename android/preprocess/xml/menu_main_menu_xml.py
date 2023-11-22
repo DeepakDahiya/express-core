46d358f12b15027308c0d9b7a8fdd82cb5a94102
deepakdahiya <dahiyadeepak123@gmail.com>
2023-11-22 15:59:43 +0530

    cleaning up menu bar

--- android/preprocess/xml/menu_main_menu_xml.py
+++ android/preprocess/xml/menu_main_menu_xml.py
@@ -22,10 +22,10 @@ def _ProcessXML(root):
     #   'android:visibility="gone" />'
     # node = ET.fromstring(node_str, parser=ET.XMLParser(encoding="utf-8"))
 
-    # parent = root.find('group/[@android:id="@+id/PAGE_MENU"]', namespaces=ns)
-    # child = parent.find('item/[@android:id="@+id/all_bookmarks_menu_id"]',
-    #                     namespaces=ns)
-    # idx = list(parent).index(child)
+    parent = root.find('group/[@android:id="@+id/PAGE_MENU"]', namespaces=ns)
+    child = parent.find('item/[@android:id="@+id/all_bookmarks_menu_id"]',
+                        namespaces=ns)
+    idx = list(parent).index(child)
     # parent.insert(idx + 1, node)
 
     # playlist_node_str = '<item xmlns:android='\
