53eb274c21a1817ec53b2968251c05cfa2e5329a
deepakdahiya <dahiyadeepak123@gmail.com>
2023-12-17 14:38:34 +0530

    partial profile

---
+++ android/java/org/chromium/chrome/browser/settings/BrowserExpressCommentsPreferencesUtil.java
@@ -0,0 +1,153 @@
+
+/* Copyright (c) 2020 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+package org.chromium.chrome.browser.settings;
+
+import android.content.Context;
+import android.os.Build;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import org.chromium.base.ContextUtils;
+import org.chromium.base.Log;
+import org.chromium.base.ThreadUtils;
+import org.chromium.base.task.AsyncTask;
+import org.chromium.chrome.browser.about_settings.AboutChromeSettings;
+import org.chromium.chrome.browser.about_settings.AboutSettingsBridge;
+import org.chromium.chrome.browser.ntp_background_images.NTPBackgroundImagesBridge;
+import org.chromium.chrome.browser.profiles.Profile;
+import org.chromium.net.ChromiumNetworkAdapter;
+import org.chromium.net.NetworkTrafficAnnotationTag;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+
+public class BrowserExpressCommentsPreferencesUtil {
+    private static final String TAG = "Login_Browser_Express";
+    private static final String LOGIN_URL = "https://api.browser.express/v1/auth/login";
+
+    public interface LoginCallback {
+        void loginSuccessful(String accessToken, String refreshToken);
+        void loginFailed(String error);
+    }
+
+    public static class LoginWorkerTask extends AsyncTask<Void> {
+        private String mEmail;
+        private String mPassword;
+        private LoginCallback mCallback;
+        private static Boolean loginStatus;
+        private static String mErrorMessage;
+        private static String mAccessToken;
+        private static String mRefreshToken;
+
+        public LoginWorkerTask(
+                String email, String password, LoginCallback callback) {
+            mEmail = email;
+            mPassword = password;
+            mCallback = callback;
+            loginStatus = false;
+            mErrorMessage = "";
+            mAccessToken = null;
+            mRefreshToken = null;
+        }
+
+        public static void setAuthTokens(String accessToken, String refreshToken){
+            mAccessToken = accessToken;
+            mRefreshToken = refreshToken;
+        }
+
+        public static void setLoginSuccessStatus(Boolean status){
+            loginStatus = status;
+        }
+
+        public static void setErrorMessage(String error){
+            mErrorMessage = error;
+        }
+
+        @Override
+        protected Void doInBackground() {
+            sendLoginRequest(mEmail, mPassword, mCallback);
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            assert ThreadUtils.runningOnUiThread();
+            if (isCancelled()) return;
+            if(loginStatus){
+                mCallback.loginSuccessful(mAccessToken, mRefreshToken);
+            }else{
+                mCallback.loginFailed(mErrorMessage);
+            }
+        }
+    }
+
+    private static void sendLoginRequest(String email, String password, LoginCallback callback) {
+        StringBuilder sb = new StringBuilder();
+        HttpURLConnection urlConnection = null;
+        try {
+            URL url = new URL(LOGIN_URL);
+            urlConnection = (HttpURLConnection) ChromiumNetworkAdapter.openConnection(
+                    url, NetworkTrafficAnnotationTag.MISSING_TRAFFIC_ANNOTATION);
+            urlConnection.setDoOutput(true);
+            urlConnection.setRequestMethod("POST");
+            urlConnection.setUseCaches(false);
+            urlConnection.setRequestProperty("Content-Type", "application/json");
+            urlConnection.connect();
+
+            JSONObject jsonParam = new JSONObject();
+            jsonParam.put("email", email);
+            jsonParam.put("platform", "Android");
+            jsonParam.put("password", password);
+
+            OutputStream outputStream = urlConnection.getOutputStream();
+            byte[] input = jsonParam.toString().getBytes(StandardCharsets.UTF_8.name());
+            outputStream.write(input, 0, input.length);
+            outputStream.flush();
+            outputStream.close();
+
+            int HttpResult = urlConnection.getResponseCode();
+            if (HttpResult == HttpURLConnection.HTTP_OK) {
+                BufferedReader br = new BufferedReader(new InputStreamReader(
+                        urlConnection.getInputStream(), StandardCharsets.UTF_8.name()));
+                String line = null;
+                while ((line = br.readLine()) != null) {
+                    sb.append(line + "\n");
+                }
+                JSONObject responseObject = new JSONObject(sb.toString());
+                if(responseObject.getBoolean("success")){
+                    LoginWorkerTask.setLoginSuccessStatus(true);
+                    String accessToken = responseObject.getString("accessToken");
+                    String refreshToken = responseObject.getString("refreshToken");
+                    LoginWorkerTask.setAuthTokens(accessToken, refreshToken);
+                }else{
+                    LoginWorkerTask.setLoginSuccessStatus(false);
+                    LoginWorkerTask.setErrorMessage(responseObject.getString("error"));
+                }
+                br.close();
+            } else {
+                Log.e(TAG, urlConnection.getResponseMessage());
+            }
+        } catch (MalformedURLException e) {
+            Log.e(TAG, e.getMessage());
+        } catch (IOException e) {
+            Log.e(TAG, e.getMessage());
+        } catch (JSONException e) {
+            Log.e(TAG, e.getMessage());
+        } catch (Exception e) {
+            Log.e(TAG, e.getMessage());
+        } finally {
+            if (urlConnection != null) urlConnection.disconnect();
+        }
+    }
+}
