45bf5d5e8ec351c2759ce0ad22d77032a0ac8051
deepakdahiya <dahiyadeepak123@gmail.com>
2023-12-17 19:52:44 +0530

    testing new bottom sheet design

--- android/java/org/chromium/chrome/browser/browser_express_generate_username/BrowserExpressGenerateUsernameBottomSheetFragment.java
+++ android/java/org/chromium/chrome/browser/browser_express_generate_username/BrowserExpressGenerateUsernameBottomSheetFragment.java
@@ -153,7 +153,7 @@ public class BrowserExpressGenerateUsernameBottomSheetFragment extends BottomShe
                 public void claimUsernameFailed(String error) {
                     Log.e("BROWSER EXPRESS LOGIN", "INSIDE LOGIN FAILED");
                     nextButton.setClickable(true);
-                    nextButton.setText(R.string.browser_express_login_button_title);
+                    nextButton.setText(R.string.brave_next);
                 }
             };
 }
