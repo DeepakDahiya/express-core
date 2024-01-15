987cecc5f448d0392a3a0fa59ebe2ba161d00e54
deepakdahiya <dahiyadeepak123@gmail.com>
2024-01-15 15:23:48 +0530

    partial get comments

--- android/java/org/chromium/chrome/browser/browser_express_comments/BrowserExpressCommentsBottomSheetFragment.java
+++ android/java/org/chromium/chrome/browser/browser_express_comments/BrowserExpressCommentsBottomSheetFragment.java
@@ -151,9 +151,11 @@ public class BrowserExpressCommentsBottomSheetFragment extends BottomSheetDialog
             new BrowserExpressGetCommentsUtil.GetCommentsCallback() {
                 @Override
                 public void getCommentsSuccessful(List<Comment> comments) {
+                    int len = mComments.length();
                     mComments.clear();
+                    adapter.notifyItemRangeRemoved(0, len);
                     mComments.addAll(comments);
-                    mCommentAdapter.notifyDataSetChanged();
+                    mCommentAdapter.notifyItemRangeInserted(0, comments.length());
                 }
 
                 @Override
