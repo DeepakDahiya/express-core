59fbaa66b63970a0944ea657e2248ac7ed786286
deepakdahiya <dahiyadeepak123@gmail.com>
2024-12-01 22:12:57 +0530

    fixing replies

--- android/java/org/chromium/chrome/browser/browser_express_comments/CommentListAdapter.java
+++ android/java/org/chromium/chrome/browser/browser_express_comments/CommentListAdapter.java
@@ -191,7 +191,7 @@ public class CommentListAdapter extends RecyclerView.Adapter {
             voteCountText.setText(String.format(Locale.getDefault(), "%d", finalVote));
             // mShowMoreButton.setVisibility(comment.getCommentCount() > 0 && !mIsReplyAdapter ? View.VISIBLE : View.GONE);
             Log.e("BROWSER_EXPRESS_REPLY_COMMENT_PARENT", comment.getCommentParent());
-            if(comment.getCommentParent() == null){
+            if(mIsReplyAdapter == false){
                 Log.e("BROWSER_EXPRESS_REPLY_COMMENT_PARENT", "NOT_FOUND");
                 mActionItemsLayout.setVisibility(View.VISIBLE);
             }else{
