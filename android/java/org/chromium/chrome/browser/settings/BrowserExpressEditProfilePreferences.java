13cac24d379d2eada7358de0678cc0f647cfcbcd
deepakdahiya <dahiyadeepak123@gmail.com>
2024-12-25 00:21:39 +0530

    testing image selector

--- android/java/org/chromium/chrome/browser/settings/BrowserExpressEditProfilePreferences.java
+++ android/java/org/chromium/chrome/browser/settings/BrowserExpressEditProfilePreferences.java
@@ -274,8 +274,8 @@ public class BrowserExpressEditProfilePreferences extends BravePreferenceFragmen
     }
 
     private void openImagePicker() {
-        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-        intent.setType("image/*");
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setDataAndType(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, "image/*");
         startActivityForResult(intent, REQUEST_IMAGE_PICK);
     }
 
