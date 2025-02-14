f18606ca2715ef874bd0a7d4e74bf20af1013661
deepakdahiya <dahiyadeepak123@gmail.com>
2025-02-14 17:30:56 +0530

    remove framelayout

--- android/java/org/chromium/chrome/browser/browser_express_comments/BrowserExpressCommentsBottomSheetFragment.java
+++ android/java/org/chromium/chrome/browser/browser_express_comments/BrowserExpressCommentsBottomSheetFragment.java
@@ -171,25 +171,25 @@ public class BrowserExpressCommentsBottomSheetFragment extends BottomSheetDialog
     }
 
     private void loadFragment(Fragment fragment) {
-        FragmentManager fragmentManager = getChildFragmentManager();
-        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        // FragmentManager fragmentManager = getChildFragmentManager();
+        // FragmentTransaction transaction = fragmentManager.beginTransaction();
 
-        transaction.setCustomAnimations(
-            R.anim.slide_in_right,  // enter
-            R.anim.slide_out_left,  // exit
-            R.anim.slide_in_left,   // popEnter
-            R.anim.slide_out_right  // popExit
-        );
+        // transaction.setCustomAnimations(
+        //     R.anim.slide_in_right,  // enter
+        //     R.anim.slide_out_left,  // exit
+        //     R.anim.slide_in_left,   // popEnter
+        //     R.anim.slide_out_right  // popExit
+        // );
 
-        transaction.replace(R.id.bottom_sheet_container, fragment).addToBackStack(null).commit();
+        // transaction.replace(R.id.bottom_sheet_container, fragment).addToBackStack(null).commit();
     }
 
     public void openReplies(String commentId) {
-        ReplyListFragment replyFragment = new ReplyListFragment();
-        Bundle args = new Bundle();
-        args.putString("comment_id", commentId);
-        replyFragment.setArguments(args);
-        loadFragment(replyFragment);
+        // ReplyListFragment replyFragment = new ReplyListFragment();
+        // Bundle args = new Bundle();
+        // args.putString("comment_id", commentId);
+        // replyFragment.setArguments(args);
+        // loadFragment(replyFragment);
     }
 
     public void dismissBottomsheet() {
