5dd2c088dfe9add2c0f6d99a731d20bc01888563
deepakdahiya <dahiyadeepak123@gmail.com>
2025-02-14 16:55:02 +0530

    revert coordinator

--- android/java/org/chromium/chrome/browser/browser_express_comments/BrowserExpressCommentsBottomSheetFragment.java
+++ android/java/org/chromium/chrome/browser/browser_express_comments/BrowserExpressCommentsBottomSheetFragment.java
@@ -141,18 +141,18 @@ public class BrowserExpressCommentsBottomSheetFragment extends BottomSheetDialog
         getActivity().getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
         int screenHeight = displayMetrics.heightPixels;
 
-        int defaultHeight = (int) (screenHeight * 0.7);
+        int defaultHeight = (int) (screenHeight * 0.8);
         int fullHeight = screenHeight;
 
-        // ViewGroup.LayoutParams params = view.getLayoutParams();
-        // params.height = fullHeight; 
-        // view.setLayoutParams(params);
+        ViewGroup.LayoutParams params = view.getLayoutParams();
+        params.height = fullHeight; 
+        view.setLayoutParams(params);
 
         // // Configure the BottomSheetBehavior
-        // behavior.setPeekHeight(defaultHeight);
-        // behavior.setFitToContents(false);
-        // behavior.setHalfExpandedRatio(0.7f);
-        // behavior.setState(BottomSheetBehavior.STATE_HALF_EXPANDED);
+        behavior.setPeekHeight(defaultHeight);
+        behavior.setFitToContents(false);
+        behavior.setHalfExpandedRatio(0.6f);
+        behavior.setState(BottomSheetBehavior.STATE_HALF_EXPANDED);
 
         getDialog().getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE);
 
