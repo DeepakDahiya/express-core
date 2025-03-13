9c39b76ee6569cfc4c674d94a6c129976eacc6fe
deepakdahiya <dahiyadeepak123@gmail.com>
2025-03-13 20:23:41 +0530

    fix back button

--- android/java/org/chromium/chrome/browser/browser_express_comments/BrowserExpressCommentsBottomSheetFragment.java
+++ android/java/org/chromium/chrome/browser/browser_express_comments/BrowserExpressCommentsBottomSheetFragment.java
@@ -157,7 +157,7 @@ public class BrowserExpressCommentsBottomSheetFragment extends BottomSheetDialog
                 if (keyCode == KeyEvent.KEYCODE_BACK && event.getAction() == KeyEvent.ACTION_UP) {
                     FragmentManager fragmentManager = getChildFragmentManager();
                     openComments();
-                    return true
+                    return true;
                     // if (fragmentManager.getBackStackEntryCount() > 1) {
                     //     // We're on the reply fragment, pop back to comments fragment
                     //     openComments();
