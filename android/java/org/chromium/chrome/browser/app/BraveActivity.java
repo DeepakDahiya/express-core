c9edf98ab38ac2436721add7ed42040639087ef3
deepakdahiya <dahiyadeepak123@gmail.com>
2025-03-15 16:58:49 +0530

    minor color

--- android/java/org/chromium/chrome/browser/app/BraveActivity.java
+++ android/java/org/chromium/chrome/browser/app/BraveActivity.java
@@ -374,8 +374,6 @@ public abstract class BraveActivity extends ChromeActivity
             BraveSetDefaultBrowserUtils.showBraveSetDefaultBrowserDialog(BraveActivity.this, true);
         }
 
-        BraveFeatureUtil.enableFeature(BraveFeatureList.ENABLE_FORCE_DARK, true, true);
-
         if(!NotificationManagerCompat.from(this).areNotificationsEnabled()){
             this.showNotificationRationale();
             // BravePermissionUtils.requestPermission(this);
