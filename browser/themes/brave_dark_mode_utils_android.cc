0bfd3251d5499cdf1015fbf19097bc2180276da1
deepakdahiya <dahiyadeepak123@gmail.com>
2025-03-22 18:36:20 +0530

    testing always dark more 4

--- browser/themes/brave_dark_mode_utils_android.cc
+++ browser/themes/brave_dark_mode_utils_android.cc
@@ -24,7 +24,7 @@ void SetUseSystemDarkModeEnabledForTest(bool enabled) {
 }
 
 std::string GetStringFromBraveDarkModeType(BraveDarkModeType type) {
-  return "Default";
+  return "Dark";
 }
 
 void SetBraveDarkModeType(const std::string& type) {
@@ -34,11 +34,11 @@ void SetBraveDarkModeType(BraveDarkModeType type) {
 }
 
 BraveDarkModeType GetActiveBraveDarkModeType() {
-  return BraveDarkModeType::BRAVE_DARK_MODE_TYPE_DEFAULT;
+  return BraveDarkModeType::BRAVE_DARK_MODE_TYPE_DARK;
 }
 
 BraveDarkModeType GetBraveDarkModeType() {
-  return BraveDarkModeType::BRAVE_DARK_MODE_TYPE_DEFAULT;
+  return BraveDarkModeType::BRAVE_DARK_MODE_TYPE_DARK;
 }
 
 base::Value::List GetBraveDarkModeTypeList() {
