312d17c8d84c97d640236060eadaf3c771d2bbd2
deepakdahiya <dahiyadeepak123@gmail.com>
2025-05-29 16:46:59 +0530

    fix image

--- android/java/org/chromium/chrome/browser/browser_express_comments/BrowserExpressCommentsBottomSheetFragment.java
+++ android/java/org/chromium/chrome/browser/browser_express_comments/BrowserExpressCommentsBottomSheetFragment.java
@@ -438,9 +438,13 @@ public class BrowserExpressCommentsBottomSheetFragment extends BottomSheetDialog
     @Override
     public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        mMessageEditText.requestFocus();
-        InputMethodManager imm = (InputMethodManager)getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
-        imm.showSoftInput(mMessageEditText, InputMethodManager.SHOW_IMPLICIT);
+        try {
+            BraveActivity activity = BraveActivity.getBraveActivity();
+            mMessageEditText.requestFocus();
+            InputMethodManager imm = (InputMethodManager)activity.getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.showSoftInput(mMessageEditText, InputMethodManager.SHOW_IMPLICIT);
+        } catch (BraveActivity.BraveActivityNotFoundException e) {
+        } 
         if (requestCode == PICK_MEDIA_REQUEST && resultCode == Activity.RESULT_OK) {
             if (data != null && data.getData() != null) {
                 Uri originalUri = data.getData();
