5e3a5efc1122713af1d8d6abc3b5def73c5d81fa
deepakdahiya <dahiyadeepak123@gmail.com>
2025-06-07 19:12:48 +0530

    media card for comments

--- android/java/org/chromium/chrome/browser/browser_express_comments/CommentListAdapter.java
+++ android/java/org/chromium/chrome/browser/browser_express_comments/CommentListAdapter.java
@@ -69,6 +69,8 @@ import androidx.annotation.NonNull;
 import com.google.android.exoplayer2.MediaMetadata;
 import com.bumptech.glide.request.target.Target;
 import android.util.TypedValue;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import android.widget.Space;
 
 public class CommentListAdapter extends RecyclerView.Adapter<CommentListAdapter.CommentHolder> {
     private Context mContext;
