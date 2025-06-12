17db13b699061bfe3fc5fb2e99423d404bd4b75c
deepakdahiya <dahiyadeepak123@gmail.com>
2025-06-12 23:41:37 +0530

    ntp fix

--- android/java/org/chromium/chrome/browser/ntp/PostListAdapter.java
+++ android/java/org/chromium/chrome/browser/ntp/PostListAdapter.java
@@ -39,7 +39,12 @@ import org.chromium.chrome.browser.local_database.TopSiteTable;
 import org.chromium.chrome.browser.util.TabUtils;
 
 import android.view.animation.Animation;
+import com.bumptech.glide.Glide;
+import org.chromium.chrome.browser.app.helpers.ImageLoader;
 import android.animation.ValueAnimator;
+import org.chromium.base.Log;
+import androidx.annotation.NonNull;
+import android.view.animation.AnimationUtils;
 
 import java.io.File;
 import java.util.ArrayList;
