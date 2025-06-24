b84114edd2f8b031fb711df4a392c5566ab62a2b
deepakdahiya <dahiyadeepak123@gmail.com>
2025-06-24 22:23:13 +0530

    trying to remove tab grouping

--- android/java/org/chromium/chrome/browser/settings/BraveMainPreferencesBase.java
+++ android/java/org/chromium/chrome/browser/settings/BraveMainPreferencesBase.java
@@ -314,8 +314,8 @@ public class BraveMainPreferencesBase
         int displaySectionOrder = generalOrder;
         findPreference(PREF_DISPLAY_SECTION).setOrder(++displaySectionOrder);
 
-        findPreference(PREF_MEDIA).setOrder(++displaySectionOrder);
-        // findPreference(PREF_APPEARANCE).setOrder(++displaySectionOrder);
+        // findPreference(PREF_MEDIA).setOrder(++displaySectionOrder);
+        findPreference(PREF_APPEARANCE).setOrder(++displaySectionOrder);
         // findPreference(PREF_NEW_TAB_PAGE).setOrder(++displaySectionOrder);
         findPreference(PREF_ACCESSIBILITY).setOrder(++displaySectionOrder);
         findPreference(PREF_BRAVE_LANGUAGES).setOrder(++displaySectionOrder);
