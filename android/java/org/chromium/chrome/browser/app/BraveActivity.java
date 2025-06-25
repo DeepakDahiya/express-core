907aab89a508d5b232c762f55bfd7d19558d752b
deepakdahiya <dahiyadeepak123@gmail.com>
2025-06-25 10:07:36 +0530

    NEW HOME PIP

--- android/java/org/chromium/chrome/browser/app/BraveActivity.java
+++ android/java/org/chromium/chrome/browser/app/BraveActivity.java
@@ -256,6 +256,8 @@ import android.content.pm.PackageInfo;
 import org.chromium.chrome.browser.toolbar.bottom.BrowserExpressGetLatestApkUtil;
 import org.chromium.base.task.AsyncTask;
 
+import android.webkit.JavascriptInterface;
+
 /**
  * Brave's extension for ChromeActivity
  */
