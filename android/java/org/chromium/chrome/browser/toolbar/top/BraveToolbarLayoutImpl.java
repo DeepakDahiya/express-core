aeb6df35f3bebbd08c0d3e7a5bdf696e8ffb7925
deepakdahiya <dahiyadeepak123@gmail.com>
2025-06-25 18:43:50 +0530

    fix cancel button

--- android/java/org/chromium/chrome/browser/toolbar/top/BraveToolbarLayoutImpl.java
+++ android/java/org/chromium/chrome/browser/toolbar/top/BraveToolbarLayoutImpl.java
@@ -583,10 +583,27 @@ public abstract class BraveToolbarLayoutImpl extends ToolbarLayout
 
                 try {
                     BraveActivity activity = BraveActivity.getBraveActivity();
-
+                    String accessToken = activity.getAccessToken();
+                    
                     int commentCount = 0;
                     mCommentsText = activity.getCommentCountText();
                     mCommentsText.setText(String.format(Locale.getDefault(), "%d comments", commentCount));
+
+                    if (accessToken != null) {
+                        Context context = ContextUtils.getApplicationContext();
+                        SharedPreferences prefs = context.getSharedPreferences(BE_PROFILE_PREF, 0);
+                        String avatar = prefs.getString("avatar_url", null);
+                        JSONObject decodedAccessTokenObj = getDecodedToken(accessToken);
+                        if (avatar != null) {
+                            ImageLoader.downloadImage(avatar, Glide.with(getContext()), true, 5, mProfileButton, null);
+                        }else{
+                            ImageLoader.downloadImage("https://api.dicebear.com/9.x/fun-emoji/png?seed=" + decodedAccessTokenObj.getString("_id") + "&radius=50&backgroundColor=059ff2,71cf62,d84be5,d9915b,f6d594,fcbc34,ffd5dc,ffdfbf,b6e3f4,c0aede,d1d4f9&backgroundType=gradientLinear&mouth=cute,faceMask,kissHeart,lilSmile,smileLol,smileTeeth,tongueOut,wideSmile", Glide.with(getContext()), true, 5, mProfileButton, null);
+                        }
+
+                        BrowserExpressGetProfilePreferencesUtil.GetProfileWorkerTask workerTask1 =
+                            new BrowserExpressGetProfilePreferencesUtil.GetProfileWorkerTask(accessToken, getProfileCallback);
+                        workerTask1.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
+                    }
                 } catch (BraveActivity.BraveActivityNotFoundException e) {
                     Log.e(TAG, "BookmarkButton click " + e);
                 }
