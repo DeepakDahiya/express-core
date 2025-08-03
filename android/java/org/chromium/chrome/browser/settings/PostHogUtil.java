da19b1c2f51690a52ba7473c4287eb7633c92405
deepakdahiya <dahiyadeepak123@gmail.com>
2025-08-04 00:51:30 +0530

    Posthog setup

--- android/java/org/chromium/chrome/browser/settings/PostHogUtil.java
+++ android/java/org/chromium/chrome/browser/settings/PostHogUtil.java
@@ -30,7 +30,7 @@ import java.lang.Integer;
 
 public class PostHogUtil {
     private static final String TAG = "Post_Hog_Util";
-    private static final String GET_PROFILE_URL = "https://us.i.posthog.com";
+    private static final String GET_PROFILE_URL = "https://us.i.posthog.com/i/v0/e/";
     private static final String POST_HOG_API_KEY = "phc_Nn8HVrhAeFqo35ymuvUKOKe59RhXnShdushDuyDn3vL";
 
     public static class PostHogWorkerTask extends AsyncTask<Void> {
