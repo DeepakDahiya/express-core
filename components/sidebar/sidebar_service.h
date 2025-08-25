fffb020956ddb4a00b47c32e11e9a72f5a03c641
deepakdahiya <dahiyadeepak123@gmail.com>
2025-08-26 00:56:31 +0530

    strings

--- components/sidebar/sidebar_service.h
+++ components/sidebar/sidebar_service.h
@@ -43,23 +43,6 @@ class SidebarService : public KeyedService {
     kShowNever,
   };
 
-  // This is the default display order
-  static constexpr SidebarItem::BuiltInItemType kDefaultBuiltInItemTypes[] = {
-      SidebarItem::BuiltInItemType::kBraveTalk,
-      SidebarItem::BuiltInItemType::kWallet,
-      SidebarItem::BuiltInItemType::kChatUI,
-      SidebarItem::BuiltInItemType::kBookmarks,
-      SidebarItem::BuiltInItemType::kReadingList,
-      SidebarItem::BuiltInItemType::kHistory,
-      SidebarItem::BuiltInItemType::kPlaylist};
-  static_assert(
-      std::size(kDefaultBuiltInItemTypes) ==
-          static_cast<size_t>(SidebarItem::BuiltInItemType::kBuiltInItemLast),
-      "A built-in item in this visual order is missing or you might forget to "
-      "update kBuiltInItemItemLast value. If you want to add a "
-      "new item while keeping that hidden, please visit "
-      "GetBuiltInItemForType() in sidebar_service.cc");
-
   class Observer : public base::CheckedObserver {
    public:
     virtual void OnItemAdded(const SidebarItem& item, size_t index) {}
@@ -77,7 +60,9 @@ class SidebarService : public KeyedService {
   static void RegisterProfilePrefs(PrefRegistrySimple* registry,
                                    version_info::Channel channel);
 
-  explicit SidebarService(PrefService* prefs);
+  SidebarService(
+      PrefService* prefs,
+      const std::vector<SidebarItem::BuiltInItemType>& default_builtin_items);
   ~SidebarService() override;
 
   const std::vector<SidebarItem>& items() const { return items_; }
@@ -110,6 +95,10 @@ class SidebarService : public KeyedService {
   FRIEND_TEST_ALL_PREFIXES(SidebarServiceTest, AddRemoveItems);
   FRIEND_TEST_ALL_PREFIXES(SidebarBrowserTest, ItemAddedBubbleAnchorViewTest);
   FRIEND_TEST_ALL_PREFIXES(SidebarBrowserTest, ItemAddedScrollTest);
+  FRIEND_TEST_ALL_PREFIXES(SidebarBrowserTest,
+                           DisabledItemsTestWithGuestWindow);
+
+  static bool IsDisabledItemForGuest(SidebarItem::BuiltInItemType type);
 
   void LoadSidebarItems();
   void UpdateSidebarItemsToPrefStore();
@@ -121,6 +110,11 @@ class SidebarService : public KeyedService {
   void MigrateSidebarShowOptions();
   void MigratePrefSidebarBuiltInItemsToHidden();
 
+  SidebarItem::BuiltInItemType GetPrevBuiltInItemFor(
+      const SidebarItem::BuiltInItemType& item) const;
+  size_t GetBuiltInItemIndexToInsert(const std::vector<SidebarItem>& items,
+                                     const SidebarItem& item) const;
+
   void AddItemAtForTesting(const SidebarItem& item, size_t index);
 
   raw_ptr<PrefService> prefs_ = nullptr;
@@ -129,6 +123,7 @@ class SidebarService : public KeyedService {
 
   p3a::SidebarP3A sidebar_p3a_;
 
+  const std::vector<SidebarItem::BuiltInItemType> default_builtin_items_;
   base::ObserverList<Observer> observers_;
   PrefChangeRegistrar pref_change_registrar_;
 };
