1eb6ef3c916d2f3f6f166ef8152c482e7cadc2e0
deepakdahiya <dahiyadeepak123@gmail.com>
2025-08-26 01:46:02 +0530

    partial chromoium_src

--- chromium_src/components/search_engines/default_search_manager.cc
+++ chromium_src/components/search_engines/default_search_manager.cc
@@ -9,14 +9,14 @@
 
 namespace {
 
-// bool IsDefaultSearchProviderByExtension(PrefService* pref_service) {
-//   // |kDefaultSearchProviderByExtension| is only used by desktop.
-// #if BUILDFLAG(IS_ANDROID)
-//   return false;
-// #else
-//   return pref_service->GetBoolean(prefs::kDefaultSearchProviderByExtension);
-// #endif
-// }
+bool IsDefaultSearchProviderByExtension(PrefService* pref_service) {
+  // |kDefaultSearchProviderByExtension| is only used by desktop.
+#if BUILDFLAG(IS_ANDROID)
+  return false;
+#else
+  return pref_service->GetBoolean(prefs::kDefaultSearchProviderByExtension);
+#endif
+}
 
 }  // namespace
 
@@ -48,7 +48,7 @@ namespace {
 
 // clang-format off
 #define LOADDEFAULTSEARCHENGINEFROMPREFS_BRAVE                      \
-  } else if (false)  {  \
+  } else if (IsDefaultSearchProviderByExtension(pref_service_))  {  \
     extension_default_search_ = std::move(turl_data);
 // clang-format on
 
