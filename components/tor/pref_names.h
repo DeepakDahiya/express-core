f62898de7340ab78037ea1e916920e0da8a130c4
deepakdahiya <dahiyadeepak123@gmail.com>
2025-08-26 01:12:41 +0530

    fixed components

--- components/tor/pref_names.h
+++ components/tor/pref_names.h
@@ -8,11 +8,15 @@
 
 namespace tor::prefs {
 
-extern const char kTorDisabled[];
+inline constexpr char kTorDisabled[] = "tor.tor_disabled";
+inline constexpr char kBridgesConfig[] = "tor.bridges";
 
 // Automatically open onion available site or .onion domain in Tor window
-extern const char kAutoOnionRedirect[];
-extern const char kBridgesConfig[];
+inline constexpr char kAutoOnionRedirect[] = "tor.auto_onion_location";
+
+// Restrict requests for .onion URLs to Tor windows
+inline constexpr char kOnionOnlyInTorWindows[] =
+    "tor.onion_only_in_tor_windows";
 
 }  // namespace tor::prefs
 
