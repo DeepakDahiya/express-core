19c7f2059f896f02cb41238b1a8394a00199dc66
deepakdahiya <dahiyadeepak123@gmail.com>
2025-09-04 11:29:59 +0530

    global pip player

--- browser/android/youtube_script_injector/java/org/chromium/chrome/browser/BraveYouTubeScriptInjectorNativeHelper.java
+++ browser/android/youtube_script_injector/java/org/chromium/chrome/browser/BraveYouTubeScriptInjectorNativeHelper.java
@@ -18,6 +18,7 @@ import org.chromium.content_public.browser.MediaSession;
 import org.chromium.content_public.browser.WebContents;
 import org.chromium.ui.base.WindowAndroid;
 import android.view.Surface;
+import org.chromium.chrome.browser.app.BraveActivity;
 
 /**
  * Helper to interact with native methods. Check brave_youtube_script_injector_native_helper.{h|cc}.
